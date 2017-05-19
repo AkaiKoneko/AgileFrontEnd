3791906d1648da4b4faa719acbe3efa84e26cc47
Akai <romek1248@gmail.com>
2017-05-19 23:44:38 +0200

    Czesciowy rest springa

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -3,13 +3,44 @@
  */
 
 import { Injectable } from '@angular/core';
+import { Http, Response , HttpModule } from '@angular/http'
+
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 import{ Task } from './task';
 import  { TASKS } from './mock-task';
 
 @Injectable()
 export class TaskService {
-  getTasks(): Promise<Task[]> {
-    return Promise.resolve(TASKS);
+
+  private taskUrl = 'http://localhost:8080/task.json';
+
+  constructor(private http: Http) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get(this.taskUrl)
+      .map((response: Response) => <Task[]>response.json())
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 }
